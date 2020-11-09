c0a3d5471b1879f865ed715f7c95bda78572ba2a
maximchuk35 <61186003+maximchuk35@users.noreply.github.com>
2020-11-09 04:50:47 +0200

    добавив Зворотній звязок на Contact через mailKid

--- BethanysPieShop/Controllers/ContactController.cs
+++ BethanysPieShop/Controllers/ContactController.cs
@@ -1,19 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using Microsoft.Extensions.Logging;
+using BethanysPieShop.Models;
+using MailKit;
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace BethanysPieShop.Controllers
 {
+   
+
     public class ContactController : Controller
     {
-        // GET: /<controller>/
+       
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult SendMail(string name, string email, string msg)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("ommaksymchuk.fitu18@kubg.edu.ua"));
+            message.To.Add(new MailboxAddress("maximchuk3@gmail.com"));
+            message.Subject = name;
+            message.Body = new TextPart("html")
+            {
+                Text = "Імя :" + name + "<br> " +
+                "Email: " + email + "<br>" +
+                "Повідомлення :" + msg
+            };
+
+            using(var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 465);
+                client.Authenticate("ommaksymchuk.fitu18@kubg.edu.ua", "maximchuk123");
+                client.Send(message);
+                client.Disconnect(false);
+            }
+
+            return View("Index");
+
+
+        }
+
+      
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+  
     }
 }
